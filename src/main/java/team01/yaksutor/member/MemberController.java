77a26f6de0edca10aa66720f23beaf09b586b0f8
최성식 <162602047+sssungsik@users.noreply.github.com>
2024-06-21 17:20:35 +0900

    pharmMain까지

--- src/main/java/team01/yaksutor/member/MemberController.java
+++ src/main/java/team01/yaksutor/member/MemberController.java
@@ -1,7 +1,15 @@
 package team01.yaksutor.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+    @GetMapping("pharm/pharmMain")
+    public String memberMain(Model model) {
+
+        return "pharm/pharmMain";
+    }
+
 }
