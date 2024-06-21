5e422ddea8b7dfde823eebdfbe09fbe2f0831d2c
최성식 <162602047+sssungsik@users.noreply.github.com>
2024-06-21 14:29:54 +0900

    d

---
+++ src/main/java/team01/yaksutor/YaksutorApplication.java
@@ -0,0 +1,13 @@
+package team01.yaksutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaksutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaksutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team01/yaksutor/YaksutorApplicationTests.java
@@ -0,0 +1,13 @@
+package team01.yaksutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaksutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
