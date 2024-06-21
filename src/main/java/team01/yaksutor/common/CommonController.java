a182eb7bf47e7d921843cf9ed46a5ede3d71da29
SXYXH <bacxr1945@gmail.com>
2024-06-21 16:52:01 +0900

    index 파일경로 수정

--- src/main/java/team01/yaksutor/common/CommonController.java
+++ src/main/java/team01/yaksutor/common/CommonController.java
@@ -1,13 +1,18 @@
 package team01.yaksutor.common;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommonController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("title", "메인화면");
+        model.addAttribute("content", "Spring boot Mybatis");
+
 
         return "index";
     }
